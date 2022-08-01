53539b77d2ec0cb399e67a1d444ffee6ae3f2086
Gustavo Mex <gustavoppymex@gmail.com>
2022-08-01 01:23:20 -0500

    ⚡feat(main.py): Added titles and descriptions for path operations

--- main.py
+++ main.py
@@ -33,7 +33,9 @@ def home():
 
 # Add new LOL champion
 @app.post('/champs/new')
-def create_champ(champion: Champion = Body(...)):
+def create_champ(
+    champion: Champion = Body(...),
+):
     return champion
 
 
@@ -46,10 +48,23 @@ def show_champ(
     # Query parameters always should be optionals
     # In this case, we expect a string field and its length should be smaller than 50.
     # Its defaul value is None.
-    name: Optional[str] = Query(None, max_length=50),
+    name: Optional[str] = Query(
+        None,
+        max_length=50,
+        title = "Champ's name",
+        description = "Enter the champ's name"
+    ),
     # In this case, this query parameter is required
-    role: Optional[str] = Query(...),
-    difficulty: Optional[str] = Query(...)
+    role: Optional[str] = Query(
+        ...,
+        title = "Champ's role",
+        description = "Enter the champ's role"
+    ),
+    difficulty: Optional[str] = Query(
+        ...,
+        title = "Champ's difficulty",
+        description = "Enter the champ's difficulty"
+    )
 ):
     return {'details': {'name': name, 'role': role, 'difficulty': difficulty}}
 
@@ -57,6 +72,11 @@ def show_champ(
 @app.get('/champs/detail/{champ_id}')
 def show_champ_by_id(
     # Example; gt equals greater than
-    champ_id: int = Path(..., gt=0)
+    champ_id: int = Path(
+        ...,
+        gt=0,
+        title = "Champ's id",
+        description = "Enter the champ's id"
+    )
 ):
     return {'details': {'id': champ_id, 'name': 'example', 'role': 'example'}}
