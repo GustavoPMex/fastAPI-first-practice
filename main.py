bcad81f4100740f771eba41501ed6509a57d8d98
Gustavo Mex <gustavoppymex@gmail.com>
2022-08-01 00:21:33 -0500

    ⚡feat(main.py): Added modal and new paths

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
 # main.py
-from fastapi import FastAPI
+# Python
+from typing import Optional
 
-# Instancia de la clase FastAPI
+# Pydantic
+from pydantic import BaseModel
+
+# FastAPI
+from fastapi import FastAPI, Body
+
+
+# Instance
 app = FastAPI()
 
-# Path operation decorator
-@app.get("/")
-# Path operation function
+
+# -- Models --
+class Champion(BaseModel):
+    # Field's Name  | Field's Type
+    name: str
+    role: str
+    difficulty: str
+    # Optional Field
+    ultimate: Optional[bool] = None
+
+# -- Paths --
+# Path operation decorator init
+@app.get('/')
+
+#  Home
 def home():
     return {"first": "practice"}
+
+# Get LOL champion
+@app.get('/champs/{champ_id}')
+async def get_champ(champ_id: int):
+    return {"champ_id": champ_id}
+
+# Add new LOL champion
+@app.post('/champs/new')
+def create_champ(champion: Champion = Body(...)):
+    return champion
